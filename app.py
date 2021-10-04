6c029b2535047a1821dbbf1b5dbd737a0370e94e
Ricardo <60947627+RicardoCarrillp@users.noreply.github.com>
2021-10-03 21:40:07 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,63 +1,88 @@
-from flask import Flask, jsonify, request
+from flask import Flask
+from flask_restx import Api, Resource, fields
+from werkzeug.middleware.proxy_fix import ProxyFix
 import random
+app = Flask(__name__)
+app.wsgi_app = ProxyFix(app.wsgi_app)
+api = Api(app, version='1.0', title='IMC App API',
+    description='A simple IMC APP API',
+)
 
-app = Flask('__name__')
+ns = api.namespace('userinfo', description='userInfo operations')
 
-usersinfo = [
+userInfo = api.model('userInfo', {    
+    'id': fields.Integer(readonly=True,required=True, description='The user unique identifier'),
+    'name': fields.String(required=True, description='The user name'),
+    'weight': fields.Integer(required=True, description='The user weight'),
+    'height': fields.Integer(required=True, description='The user height'),
+
+})
+
+
+
+class userDAO(object):
+    def __init__(self):
+        self.counter = 0
+        self.usersinfo = [
     {
         'id': random.randint(0,100) ,
         'name': 'Ricardo',
         'weight': 60,
         'height': 160,
-        'imc': 23.5
         
         
     }
 ]
 
+    def get(self, id):
+        for user in self.usersinfo:
+            if user['id'] == id:
+                return user
+        api.abort(404, "User {} doesn't exist".format(id))
 
-@app.route('/userinfo', methods=['POST'])
-def create_userinfo():
-    request_data = request.get_json()
-    w=request_data['weight'] 
-    h= request_data['height']
-    h2=h*h
-    imc=w/h2
-    print(imc)
-
-    new_user = {
-
-        'id': random.randint(0,100),
-        'name': request_data['name'],
-        'height': request_data['height'],
-        'weight': request_data['weight'],
-        'imc':imc
-        
-        
-    }
- 
-    usersinfo.append(new_user)
-    return jsonify(new_user)
+    def create(self, data):
+        user = data
+      
+        new_user = {
+            'id': random.randint(0,100),
+            'name': user['name'],
+            'height': user['height'],
+            'weight': user['weight'],
+         
+                
+                
+            }
+        self.usersinfo.append(new_user)
+        return user
 
 
-@app.route('/userinfo/<int:id>', methods=['GET'])
-def get_userinfo(id):
-    for user in usersinfo:
-        if user['id'] == id:  
-            
-            return jsonify(user)
-    return jsonify({'message': 'user not found'})
 
 
-@app.route('/userinfo')
-def get_usersinfo():
-    return jsonify({'Users': usersinfo})
+DAO = userDAO()
 
-@app.route('/')
-def get():
-    return "hello"
+
+
+
+@ns.route('/')
+class TodoList(Resource):
+    '''Shows a list of all users'''
+    @ns.doc('list user info')
+    @ns.marshal_list_with(userInfo)
+    def get(self):
+        '''List user information'''
+        return DAO.usersinfo
+
+    @ns.doc('create_todo')
+    @ns.expect(userInfo)
+    @ns.marshal_with(userInfo, code=201)
+    def post(self):
+        '''Create user information'''
+        return DAO.create(api.payload), 201
+
+
+
 
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
